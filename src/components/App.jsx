e242989566a061094c7af2e716b491b257be4f88
Morozov-it <morozov.it2020@gmail.com>
2022-01-17 14:54:26 +0300

    Set of primary state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import './app.css'
+import { useDispatch, useSelector } from "react-redux";
+import { setCount } from '../reducers/reposReducer';
+
 
 const App = () => {
+    //хук для вызова метода dispatch в redux-store
+    const dispatch = useDispatch();
+
+    //хук для получения данных из store
+    const count = useSelector(state => state.repos.count);
+
+    //функция для вызова конкретного action
+    function onCount() {
+        dispatch(setCount(1))
+    };
+
     return (
         <div className='app'>
             <h1>React app</h1>
+            <div>{ count }</div>
+            <button onClick={ ()=>onCount() }>Count</button>
         </div>
     )
 }
